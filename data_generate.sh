d73bd7a8c3bf36a058686c6c314318d64b749165
markjoonho <j669lee@uwaterloo.ca>
2025-03-13 16:40:14 +0900

    change data folder

--- data_generate.sh
+++ data_generate.sh
@@ -1,11 +1,15 @@
+python ./data_generation/split_data.py --data_dir ./train \
+--output_dir ./total_dataset \
+--train_ratio 0.8
+
 python ./data_generation/negative_data_generate.py \
---input_folder ./train \
---output_folder ./train_dataset/negative
+--input_folder ./total_dataset/train \
+--output_folder ./total_dataset/train_dataset/negative
 
 python ./data_generation/positive_data_generate.py \
---negative_input_folder ./train_dataset/negative \
---candidate_input_folder ./train \
---output_folder ./train_dataset/positive \
+--negative_input_folder ./total_dataset/train_dataset/negative \
+--candidate_input_folder ./total_dataset/train \
+--output_folder ./total_dataset/train_dataset/positive \
 --target_counts 2 3 4 5 6 7 8 9 10
 
-cp -r ./train/* ./train_dataset/positive/
+cp -r ./total_dataset/train/* ./total_dataset/train_dataset/positive/
